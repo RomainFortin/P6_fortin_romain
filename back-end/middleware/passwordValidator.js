c9fca0d902f6a522ba77ae00f7e3db86352a59b1
Romain Fortin <fortin.romain@gmail.com>
2021-07-05 14:46:24 +0200

    change DB user

--- back-end/middleware/passwordValidator.js
+++ back-end/middleware/passwordValidator.js
@@ -2,7 +2,7 @@ const passwordSchema = require('../models/passwordValidator');
 
 module.exports = (req, res, next) => {
     if (!passwordSchema.validate(req.body.password)) {
-        res.writeHead(400, 'Mot de passe requis : 8 caractères minimun. Au moins 1 Majuscule, 1 minuscule, 1 chiffre. Sans espaces', {
+        res.writeHead(400, 'Mot de passe requis : 6 caractères minimun. Au moins 1 Majuscule, 1 minuscule, 1 chiffre. Sans espaces', {
             'content-type': 'application/json'
         });
         res.end('Format de mot de passe incorrect.');
